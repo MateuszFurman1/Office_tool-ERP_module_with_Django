a0ea1d2b9800beec5adc10e22eb7c9f219f30ffb
MateuszFurman1 <106817902+MateuszFurman1@users.noreply.github.com>
2022-12-17 16:08:52 +0100

    manage view test

--- office_tool_app/conftest.py
+++ office_tool_app/conftest.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import pytest
 from django.contrib.auth.models import Permission
-from office_tool_app.models import Delegation, Vacation, MedicalLeave, AddressHome, AddressCore, User, Group
+from office_tool_app.models import Delegation, Vacation, MedicalLeave, \
+    AddressHome, AddressCore, User, Group, Messages
 
 
 @pytest.fixture
 def users():
-    name_lst = ['Tadeusz', 'Marek', 'Janusz', 'Franciszek', 'Julius', 'Adrian']
+    name_lst = ['Arkadiusz', 'Marek', 'Janusz', 'Franciszek', 'Julius', 'Adrian']
     lst = []
     for user in range(6):
         lst.append(User.objects.create(username=name_lst[user-1]))
@@ -31,7 +32,7 @@ def delegations(users):
     today = str(datetime.now().date())
     lst = []
     for n in range(5):
-        p = Delegation.objects.create(employee=users[n-1], start_date=today, end_date=today, delegation_country='DE',
+        p = Delegation.objects.create(employee=users[0], start_date=today, end_date=today, delegation_country='DE',
                                       status='pending')
         lst.append(p)
     return lst
@@ -42,7 +43,7 @@ def vacations(users):
     today = str(datetime.now().date())
     lst = []
     for n in range(5):
-        p = Vacation.objects.create(employee=users[n], replacement=users[n+1], vacation_from=today, vacation_to=today,
+        p = Vacation.objects.create(employee=users[0], replacement=users[n+1], vacation_from=today, vacation_to=today,
                                     status='pending')
         lst.append(p)
     return lst
@@ -53,7 +54,18 @@ def medical_leaves(users):
     today = str(datetime.now().date())
     lst = []
     for n in range(5):
-        p = MedicalLeave.objects.create(employee=users[n-1], from_date=today, to_date=today)
+        p = MedicalLeave.objects.create(employee=users[0], from_date=today, to_date=today)
+        lst.append(p)
+    return lst
+
+
+@pytest.fixture
+def message(users):
+    today = str(datetime.now().date())
+    lst = []
+    for n in range(5):
+        p = Messages.objects.create(from_employee=users[n+1], to_employee=users[0],
+                                    sending_date=today, message='test')
         lst.append(p)
     return lst
 
--- office_tool_app/tests.py
+++ office_tool_app/tests.py
@@ -17,10 +17,6 @@ def test_Home_view(user):
     url = reverse('home')
     response = client.get(url)
     assert response.status_code == 200
-    # persons_context = response.context['rooms']
-    # assert persons_context.count() == len(rooms)
-    # for p in rooms:
-    #     assert p in persons_context
 
 
 @pytest.mark.django_db
@@ -137,8 +133,7 @@ def test_profile_post(user):
     assert 302 == response.status_code
     assert len(User.objects.all()) != 0
 
-#Nie creationVacation post oraz nie zrobiłem
-# dla vacation detail, vacation accept, vaccation reject
+
 @pytest.mark.django_db
 def test_vacationDetail_view(users, vacations):
     client = Client()
@@ -161,8 +156,7 @@ def test_createVacation_view(user):
     form = response.context['form']
     assert isinstance(form, VacationForm)
 
-#Nie creationVacation post oraz nie zrobiłem
-# dla vacation detail, vacation accept, vaccation reject
+#Do poprawy !!!!
 @pytest.mark.django_db
 def test_createVacation_post_view(users):
     today = str(datetime.now().date())
@@ -202,8 +196,7 @@ def test_createDelegation_view(user):
     form = response.context['form']
     assert isinstance(form, DelegationForm)
 
-#Nie creationVacation post oraz nie zrobiłem
-# dla vacation detail, vacation accept, vaccation reject
+
 @pytest.mark.django_db
 def test_createDelegation_post_view(users):
     today = str(datetime.now().date())
@@ -232,7 +225,7 @@ def test_medicalLeave_view(users, medical_leaves):
     for m in medical_leaves:
         assert m in medical_context
 
-
+#Błąd!!!!
 @pytest.mark.django_db
 def test_createMedicalLeave_view(user_with_permission):
     client = Client()
@@ -243,8 +236,7 @@ def test_createMedicalLeave_view(user_with_permission):
     form = response.context['form']
     assert isinstance(form, MedicalLeaveForm)
 
-#Nie creationVacation post oraz nie zrobiłem
-# dla vacation detail, vacation accept, vaccation reject
+#Błąd!!!!
 @pytest.mark.django_db
 def test_createMedicalLeave_post_view(user_with_permission):
     today = str(datetime.now().date())
@@ -261,7 +253,7 @@ def test_createMedicalLeave_post_view(user_with_permission):
 
 
 @pytest.mark.django_db
-def test_medicalLeave_view(users, medical_leaves):
+def test_medicalLeave_delete_view(users, medical_leaves):
     client = Client()
     url = reverse('delete-medical', args=(medical_leaves[0].pk, ))
     client.force_login(users[0])
@@ -271,246 +263,41 @@ def test_medicalLeave_view(users, medical_leaves):
     assert 200 == response.status_code
     assert medical_leaves[0] == medical_context
 
-# @pytest.mark.django_db
-# def test_create_room_get_view(user):
-#     client = Client()
-#     url = reverse('create-room')
-#     client.force_login(user)
-#     response = client.get(url)
-#     assert 200 == response.status_code
-#     assert isinstance(response.context['form'], RoomForm)
-#
-#
-# @pytest.mark.django_db
-# def test_create_room_post_view_with_log(user):
-#     client = Client()
-#     url = reverse('create-room')
-#     client.force_login(user)
-#     data = {
-#         'name': 'room',
-#         'seats': 100,
-#         'projector': False
-#     }
-#     response = client.post(url, data)
-#     assert response.status_code == 302
-#     assert Room.objects.get(name='room', seats=100, projector=False)
-#
-#
-#
-#
-#
-# @pytest.mark.django_db
-# def test_edit_room_get_view_with_perm(user, rooms):
-#     client = Client()
-#     url = reverse('edit-room')
-#     client.force_login(user)
-#     room = rooms[0]
-#     data = {
-#         'id': room.id,
-#         'name': 'room',
-#         'seats': 100,
-#         'projector': False
-#     }
-#     response = client.get(url, data)
-#     assert 200 == response.status_code
-#     assert isinstance(response.context['form'], RoomForm)
-#
-#
-# @pytest.mark.django_db
-# def test_edit_room_post_view_with_perm(user, rooms):
-#     client = Client()
-#     url = reverse('edit-room')
-#     client.force_login(user)
-#     room = rooms[0]
-#     data = {
-#         'id': room.id,
-#         'name': 'room',
-#         'seats': 100,
-#         'projector': False
-#     }
-#     response = client.post(url, data)
-#     assert response.status_code == 302
-#     assert Room.objects.get(name='room', seats=100, projector=False)
-#
-#
-# @pytest.mark.django_db
-# def test_delete_room_get_view_with_perm(user, rooms):
-#     client = Client()
-#     url = reverse('delete-room')
-#     client.force_login(user)
-#     room = rooms[0]
-#     data = {
-#         'id': room.id,
-#         'name': 'room',
-#         'seats': 100,
-#         'projector': False
-#     }
-#     response = client.get(url, data)
-#     assert response.status_code == 302
-#     assert len(Room.objects.all()) == 9  # przekazujemy 10 objektów i jeden usuwamy
-#
-#
-# @pytest.mark.django_db
-# def test_reservation_room_get_view_with_perm(user, rooms):
-#     client = Client()
-#     url = reverse('room-reserve')
-#     client.force_login(user)
-#     room = rooms[0]
-#     data = {
-#         'id': room.id,
-#     }
-#     response = client.get(url, data)
-#     assert response.status_code == 200
-#     assert isinstance(response.context['form'], ReservationForm)
-#
-#
-# @pytest.mark.django_db
-# def test_reservation_room_post_view_with_perm(user, rooms):
-#     client = Client()
-#     url = reverse('room-reserve')
-#     client.force_login(user)
-#     room = rooms[0]
-#     data = {
-#         'id': room.id,
-#         'date': date.today(),
-#         'comment': 'test'
-#     }
-#     response = client.post(url, data)
-#     assert response.status_code == 302
-#     assert Reservation.objects.get(room=room)
-#
-#
-# @pytest.mark.django_db
-# def test_reservation_room_post_view_invalid_with_perm(user, rooms):
-#     client = Client()
-#     url = reverse('room-reserve')
-#     client.force_login(user)
-#     room = rooms[0]
-#     data = {
-#         'id': room.id,
-#         'room': room,
-#         'date': '2022-11-07 20:20:46.816306+01',
-#     }
-#     response = client.post(url, data)
-#     assert response.status_code == 200
-#     assert len(Reservation.objects.all()) == 0
-#     assert isinstance(response.context['form'], ReservationForm)
-#
-#
-# @pytest.mark.django_db
-# def test_detail_room_get_view(rooms):
-#     client = Client()
-#     url = reverse('detail-room')
-#     room = rooms[0]
-#     data = {
-#         'id': room.id,
-#     }
-#     response = client.get(url, data)
-#     assert 200 == response.status_code
-#
-#
-# @pytest.mark.django_db
-# def test_search_room_get_view(rooms):
-#     client = Client()
-#     url = reverse('search-room')
-#     room = rooms[0]
-#     data = {
-#         'rooms':rooms,
-#         'room-name': room.name,
-#         'seats': room.seats
-#     }
-#     response = client.get(url, data)
-#     assert 200 == response.status_code
-#
-#
 
-#
-#
-# @pytest.mark.django_db
-# def test_registration_get_view():
-#     client = Client()
-#     url = reverse('registration')
-#     response = client.get(url)
-#     assert 200 == response.status_code
-#     assert isinstance(response.context['form'], RegistrationForm)
-#
-#
-# @pytest.mark.django_db
-# def test_registration_post_valid_view():
-#     data = {
-#         'username': 'name1',
-#         'password': 'asd',
-#         're_password': 'asd'
-#     }
-#     client = Client()
-#     url = reverse('registration')
-#     response = client.post(url, data)
-#     assert 302 == response.status_code
-#     assert User.objects.get(username='name1')
-#
-#
-# @pytest.mark.django_db
-# def test_registration_post_invalid_view():
-#     data = {
-#         'username': 'name1',
-#         'password': 'asd',
-#         're_password': '111'
-#     }
-#     client = Client()
-#     url = reverse('registration')
-#     response = client.post(url, data)
-#     assert 200 == response.status_code
-#     assert len(User.objects.all()) == 0
-#     assert isinstance(response.context['form'], RegistrationForm)
-#
-#
-# @pytest.mark.django_db
-# def test_About_view(comments):
-#     client = Client()
-#     url = reverse('about')
-#     response = client.get(url)
-#     comment_context = response.context['comments']
-#     assert 200 == response.status_code
-#     assert comment_context.count() == len(comments)
-#     for p in comments:
-#         assert p in comment_context
-#
-#
-# @pytest.mark.django_db
-# def test_addcomment_room_post_view(user):
-#     client = Client()
-#     url = reverse('add-comment')
-#     client.force_login(user)
-#     data = {
-#         'text': 'Test'
-#     }
-#     response = client.post(url, data)
-#     assert 302 == response.status_code
-#     assert Comment.objects.get(text='Test')
-#
-#
-# @pytest.mark.django_db
-# def test_profile_get_view(user):
-#     client = Client()
-#     url = reverse('profile', args=(user.username, ))
-#     client.force_login(user)
-#     response = client.get(url)
-#     assert 200 == response.status_code
-#     assert isinstance(response.context['form'], UserUpdateForm)
-#
-#
-# @pytest.mark.django_db
-# def test_profile_post_valid_view(user):
-#     data = {
-#         'first_name': 'test',
-#         'last_name': 'test2',
-#         'email': 'test@test.com',
-#         'last_login': '2022-10-28 20:36:30.412291+02',
-#         'date_joined': '2022-10-28 20:36:30.412291+02',
-#     }
-#     client = Client()
-#     url = reverse('profile', args=(user.username, ))
-#     client.force_login(user)
-#     response = client.post(url, data)
-#     assert 302 == response.status_code
-#     assert len(User.objects.all()) != 0
+@pytest.mark.django_db
+def test_medicalLeave_delete_post_view(users, medical_leaves):
+    client = Client()
+    url = reverse('delete-medical', args=(medical_leaves[0].pk, ))
+    client.force_login(users[0])
+    response = client.post(url)
+    assert 302 == response.status_code
+
+
+@pytest.mark.django_db
+def test_messages_view(users, message):
+    client = Client()
+    url = reverse('messages')
+    client.force_login(users[0])
+    response = client.get(url)
+    messages_context = response.context['messages']
+    print(messages_context)
+    assert 200 == response.status_code
+    assert len(message) == messages_context.count()
+
+#Błąd!!
+@pytest.mark.django_db
+def test_Manage_view(user_with_permission):
+    client = Client()
+    url = reverse('manage')
+    client.force_login(user_with_permission)
+    response = client.get(url)
+    assert 200 == response.status_code
+
+#Błąd!!!
+@pytest.mark.django_db
+def test_ManageDetail_view(user_with_permission):
+    client = Client()
+    url = reverse('manage-detail', args=(user_with_permission.pk, ))
+    client.force_login(user_with_permission)
+    response = client.get(url)
+    assert 200 == response.status_code
